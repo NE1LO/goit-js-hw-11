e60762f3d6bc145648b09471c5327c1604db15d8
NE1LO <131876090+NE1LO@users.noreply.github.com>
2024-01-09 12:35:39 +0000

    Deploying to gh-pages from @ NE1LO/goit-js-hw-11@21ac6436c7a855e76d6c5e1250f28a70b2fd9f6e 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,29 +1,29 @@
-import{S as d,i as p}from"./assets/vendor-9310f15c.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function t(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function r(e){if(e.ep)return;e.ep=!0;const o=t(e);fetch(e.href,o)}})();const f=i=>{let s;return s=i.reduce((t,r)=>t+`<li class="photo-list__item">
-        <a href="${r.largeImageURL}">
+import{S as d,i as p}from"./assets/vendor-9310f15c.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const f=i=>{let s;return s=i.reduce((r,o)=>r+`<li class="photo-list__item">
+        <a href="${o.largeImageURL}">
           <img
             width="360"
             height="200"
-            src="${r.largeImageURL}"
-            alt="${r.tags}"
+            src="${o.largeImageURL}"
+            alt="${o.tags}"
           />
         </a>
         <div class="photo-list__block">
           <div class="photo-list__block__info">
             <p><b>Likes</b></p>
-            <p>${r.likes}</p>
+            <p>${o.likes}</p>
           </div>
           <div class="photo-list__block__info">
             <p><b>Views</b></p>
-            <p>${r.views}</p>
+            <p>${o.views}</p>
           </div>
           <div class="photo-list__block__info">
             <p><b>Comments</b></p>
-            <p>${r.comments}</p>
+            <p>${o.comments}</p>
           </div>
           <div class="photo-list__block__info">
             <p><b>Download</b></p>
-            <p>${r.downloads}</p>
+            <p>${o.downloads}</p>
           </div>
         </div>
-      </li>`,""),s},u="41690622-1f4c10e5fb0aefa04cb32f231",h="https://pixabay.com/api/",l=document.querySelector(".photo-list"),a=document.querySelector(".form"),c=document.querySelector(".loader"),m=new d(".photo-list a"),y=i=>{i.preventDefault(),l.children.length>0&&(l.innerHTML=""),c.style.display="inline-block";const s=new URLSearchParams({key:u,image_type:"photo",q:a.search.value,orientation:"horizontal",safesearch:!0});fetch(`${h}?${s}`).then(t=>t.json()).then(t=>{t.hits<1?(p.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),c.style.display="none"):(l.innerHTML=f(t.hits),c.style.display="none",m.refresh())}).catch(t=>console.log(t)),a.reset()};a.addEventListener("submit",y);
+      </li>`,""),s},u="41690622-1f4c10e5fb0aefa04cb32f231",h="https://pixabay.com/api/",l=document.querySelector(".photo-list"),a=document.querySelector(".form"),c=document.querySelector(".loader"),m=new d(".photo-list a"),y=i=>{i.preventDefault(),l.children.length>0&&(l.innerHTML=""),c.style.display="inline-block";const s=new URLSearchParams({key:u,image_type:"photo",q:a.search.value,orientation:"horizontal",safesearch:!0});fetch(`${h}?${s}`).then(r=>r.json()).then(r=>{r.hits.length===0?(p.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),c.style.display="none"):(l.innerHTML=f(r.hits),c.style.display="none",m.refresh())}).catch(r=>p.error({message:"error server",position:"topRight"})),a.reset()};a.addEventListener("submit",y);
 //# sourceMappingURL=commonHelpers.js.map
